5f038dfbabf417ef70465b012e5f79841a5c281c
theSMBD <firstsmbd@gmail.com>
2024-05-05 19:02:49 +0300

    YENI DOSYALAR VE SCREENLER

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './app/screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomeScreen></HomeScreen>
       <StatusBar style="auto" />
     </View>
   );
