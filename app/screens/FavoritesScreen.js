ef425415c257918a4ec7c2ba112619e18305fb8e
theSMBD <firstsmbd@gmail.com>
2024-06-02 16:14:54 +0300

    eklemeler yapıldı

---
+++ app/screens/FavoritesScreen.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { useStore } from '../store/store';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { COLORS, SPACING } from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import FavoritesItemCard from '../components/FavoritesItemCard';
+
+const FavoritesScreen = ({ navigation }) => {
+  const FavoritesList = useStore(state => state.FavoritesList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const addToFavoriteList = useStore(state => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(state => state.deleteFromFavoriteList);
+  const ToggleFavourite = (favourite, type, id) => {
+    favourite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+  };
+
+  return (
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, { marginBottom: tabBarHeight }]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Favourites" />
+
+            {FavoritesList.length === 0 ? (
+              <EmptyListAnimation title={'No Favourites'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {FavoritesList.map(data => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.push('Details', {
+                        index: data.index,
+                        id: data.id,
+                        type: data.type,
+                      });
+                    }}
+                    key={data.id}>
+                    <FavoritesItemCard
+                      id={data.id}
+                      imagelink_portrait={data.imagelink_portrait}
+                      name={data.name}
+                      special_ingredient={data.special_ingredient}
+                      type={data.type}
+                      ingredients={data.ingredients}
+                      average_rating={data.average_rating}
+                      ratings_count={data.ratings_count}
+                      roasted={data.roasted}
+                      description={data.description}
+                      favourite={data.favourite}
+                      ToggleFavouriteItem={ToggleFavourite}
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
+
+export default FavoritesScreen;
